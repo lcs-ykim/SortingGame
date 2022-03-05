36ce0c88d75bd33678d4ec2ada23a3900f75f642
Yeseo Kim <YKim@lcs.on.ca>
2022-03-04 21:01:42 -0500

    Initial Commit

---
+++ SortingGame/SortingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingGameApp.swift
+//  SortingGame
+//
+//  Created by Yeseo Kim on 2022-03-04.
+//
+
+import SwiftUI
+
+@main
+struct SortingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
