68aa8e704b12cd40f8bcb239d3e7aaf21f04713e
Yeseo Kim <YKim@lcs.on.ca>
2022-03-04 22:36:35 -0500

    Result and input area created on the interface

--- SortingGame/VIews/ContentView.swift
+++ SortingGame/VIews/ContentView.swift
@@ -18,6 +18,12 @@ struct ContentView: View {
     // Tracks all questions completed
     @State var completedQuizzes: [Quiz] = []
     
+    // Holds the user input
+    @State var inputGiven = ""
+
+    // Tracks whether the input has even been checked yet
+    @State var status: Status = Status.unsolved
+    
     var body: some View {
         
         VStack(spacing: 0) {
@@ -27,6 +33,29 @@ struct ContentView: View {
             
             // Displays possible answer choices
             Text("A – ... B – Compost C – ...")
+            
+            // Result and input area
+            HStack {
+                
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    // Only show this when the answer given is correct
+                        .opacity(status == Status.correct ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                    // Only show this when the answer given is correct
+                    // Neither of the images show up when the quiz is unsolved
+                        .opacity(status == Status.incorrect ? 1.0 : 0.0)
+                }
+                
+                Spacer()
+                
+                TextField("", text: $inputGiven)
+                // Ensure input is right-aligned
+                    .multilineTextAlignment(.trailing)
+            }
             
         }
 
